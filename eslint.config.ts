2fb9c01a0f13da12d640f795cff58bea12a63fbc
sangarix <denya.garkusha.12@gmail.com>
2025-10-01 16:48:28 +0000

    first git

---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,tsx,jsx}'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
+  },
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslint.configs.recommended,
+]);
